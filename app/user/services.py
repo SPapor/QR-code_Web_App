12b5fbf678716f7d0105d269bc5b3e7d6f93e251
SH21 <anosovigor404@gmail.com>
2025-03-05 17:11:40 +0200

    KAN-16: separate auth_service and user_service on register

--- app/user/services.py
+++ app/user/services.py
@@ -1,6 +1,14 @@
+from auth.services import AuthService
 from user.dal import UserRepo
+from user.models import User
 
 
 class UserService:
-    def __init__(self, user_repo: UserRepo):
+    def __init__(self, user_repo: UserRepo, auth_service: AuthService):
         self.user_repo = user_repo
+        self.auth_service = auth_service
+
+    async def register(self, username: str, password_hash: str) -> User:
+        user = User(username=username, password_hash=password_hash)
+        user = await self.user_repo.create_and_get(user)
+        return user
