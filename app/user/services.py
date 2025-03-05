abee7bf63716cc6ab99bf26b665a0590d1e76512
SH21 <anosovigor404@gmail.com>
2025-03-05 17:44:17 +0200

    cleanup: user service

--- app/user/services.py
+++ app/user/services.py
@@ -1,8 +1,6 @@
-from auth.services import AuthService
 from user.dal import UserRepo
 
 
 class UserService:
-    def __init__(self, user_repo: UserRepo, auth_service: AuthService):
+    def __init__(self, user_repo: UserRepo):
         self.user_repo = user_repo
-        self.auth_service = auth_service
