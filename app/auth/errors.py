5b8127106dc0efaf399ec70989287f7473e793e1
SH21 <anosovigor404@gmail.com>
2025-03-06 14:11:48 +0200

    refactor: introduce api errors

--- app/auth/errors.py
+++ app/auth/errors.py
@@ -1,9 +1,4 @@
-import functools
-
-from fastapi import FastAPI
-from starlette import status
-
-from core.errors import ApplicationError, static_exception_handler
+from core.errors import ApplicationError
 
 
 class AuthError(ApplicationError):
@@ -20,12 +15,3 @@ class InvalidLoginOrPasswordError(AuthError):
 
 class RefreshTokenRequiredError(AuthError):
     pass
-
-
-def register_exception_handlers(app: FastAPI):
-    seh = functools.partial(static_exception_handler, app)
-
-    seh(AuthError, status.HTTP_403_FORBIDDEN, "Auth error", "auth.0001")
-    seh(NotAuthorizedError, status.HTTP_401_UNAUTHORIZED, "Not authorized", "auth.0002")
-    seh(RefreshTokenRequiredError, status.HTTP_401_UNAUTHORIZED, "Refresh token required", "auth.0003")
-    seh(InvalidLoginOrPasswordError, status.HTTP_401_UNAUTHORIZED, "Invalid login or password", "auth.0004")
--- app/core/errors.py
+++ app/core/errors.py
@@ -1,11 +1,3 @@
-import functools
-from types import MappingProxyType
-
-from fastapi import FastAPI
-from starlette import status
-from starlette.responses import JSONResponse
-
-
 class ApplicationError(Exception):
     pass
 
@@ -18,28 +10,3 @@ class NotFoundError(ApplicationError):
 class AlreadyExistError(ApplicationError):
     def __init__(self, resource: str = "Resource"):
         super().__init__(f"{resource} already exists")
-
-
-def static_exception_handler(
-    app: FastAPI,
-    exc: type[ApplicationError],
-    status_code: int,
-    error_message: str,
-    error_code: str,
-    kwargs=MappingProxyType({}),
-):
-    @app.exception_handler(exc)
-    async def handle_error(request, e):
-        return JSONResponse(
-            status_code=status_code, content={"error_message": error_message, "error_code": error_code, **kwargs}
-        )
-
-    return handle_error
-
-
-def register_exception_handlers(app: FastAPI):
-    seh = functools.partial(static_exception_handler, app)
-
-    seh(NotFoundError, status.HTTP_404_NOT_FOUND, "Resource not found", "core.0001")
-    seh(AlreadyExistError, status.HTTP_409_CONFLICT, "Resource already exists", "core.0002")
-    seh(ApplicationError, status.HTTP_500_INTERNAL_SERVER_ERROR, "Internal server error", "core.0003")
